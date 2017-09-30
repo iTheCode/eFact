4df5c4d1a11b28cc8dede10d44b9460a81fd8319
iTheCode <ithecode.ica@gmail.com>
2017-09-30 00:35:00 -0500

    Settings up

--- eFact/settings.py
+++ eFact/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'eFact',
         'USER': 'root',
-        'PASSWORD': '81848133',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
